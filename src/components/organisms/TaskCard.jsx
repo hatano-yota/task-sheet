e4153fc0a7217fbcf8b061289c498703bc283158
unreact-hatano <mizutama.hahaha@gmail.com>
2022-12-31 16:53:55 +0900

    component作成整理

--- src/components/organisms/TaskCard.jsx
+++ src/components/organisms/TaskCard.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { TaskCardTitle } from "../molcules/TaskCardTitle";
+import { TaskCardDeleteButton } from "../molcules/TaskCardDeleteButton";
+import { TaskAddInput } from "../molcules/TaskAddInput";
+import { Tasks } from "../molcules/Tasks";
 
 export const TaskCard = () => {
-  return <div>TaskCard</div>;
+  return (
+    <div className="taskCard">
+      <TaskCardTitle />
+      <TaskCardDeleteButton />
+      <TaskAddInput />
+      <Tasks />
+    </div>
+  );
 };
