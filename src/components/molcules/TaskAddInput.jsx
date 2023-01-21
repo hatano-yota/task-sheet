2553a39e3c6307643fe695a833af7a9b3579e2cb
unreact-hatano <mizutama.hahaha@gmail.com>
2023-01-21 17:36:54 +0900

    空白のタスクを追加できない仕様を追加

--- src/components/molcules/TaskAddInput.jsx
+++ src/components/molcules/TaskAddInput.jsx
@@ -3,6 +3,9 @@ import React from "react";
 export const TaskAddInput = ({ inputText, setInputText, taskList, setTaskList }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (inputText === "") {
+      return;
+    }
     // カードを追加する
     setTaskList([...taskList, { id: taskList.length, text: inputText }]);
     setInputText("");
