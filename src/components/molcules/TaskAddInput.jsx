07c0135f547bba28f0c66a607469509929dc5139
unreact-hatano <mizutama.hahaha@gmail.com>
2023-01-21 15:01:50 +0900

    taskの追加を実装

--- src/components/molcules/TaskAddInput.jsx
+++ src/components/molcules/TaskAddInput.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 
-export const TaskAddInput = () => {
-  return <div>TaskAddInput</div>;
+export const TaskAddInput = ({ inputText, setInputText, taskList, setTaskList }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // カードを追加する
+    setTaskList([...taskList, { text: inputText }]);
+    setInputText("");
+  };
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+  
+  console.log(taskList);
+  
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="taskAddInput"
+          type="text"
+          placeholder="add a task"
+          maxLength="15"
+          autoFocus
+          onChange={handleChange}
+          value={inputText}
+        />
+      </form>
+    </div>
+  );
 };
--- src/components/organisms/TaskCard.jsx
+++ src/components/organisms/TaskCard.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { TaskCardTitle } from "../molcules/TaskCardTitle";
 import { TaskCardDeleteButton } from "../molcules/TaskCardDeleteButton";
 import { TaskAddInput } from "../molcules/TaskAddInput";
 import { Tasks } from "../molcules/Tasks";
 
 export const TaskCard = () => {
+  const [inputText, setInputText] = useState("");
+  const [taskList, setTaskList] = useState([]);
   return (
     <div className="taskCard">
       <TaskCardTitle />
       <TaskCardDeleteButton />
-      <TaskAddInput />
-      <Tasks />
+      <TaskAddInput
+        inputText={inputText}
+        setInputText={setInputText}
+        taskList={taskList}
+        setTaskList={setTaskList}
+      />
+      <Tasks inputText={inputText} taskList={taskList} />
     </div>
   );
 };
