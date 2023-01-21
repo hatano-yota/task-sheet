2929be8832d59330b50e80accbcd41954e21ce21
unreact-hatano <mizutama.hahaha@gmail.com>
2023-01-21 18:24:53 +0900

    タスクを並び替える挙動の実装 idによるバグ修正

--- src/components/molcules/Tasks.jsx
+++ src/components/molcules/Tasks.jsx
@@ -3,15 +3,25 @@ import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import { Task } from "../atoms/Task";
 
 export const Tasks = ({ taskList, setTaskList }) => {
+  const reorder = (dragList, startIndex, endIndex) => {
+    // 並び替える
+    const remove = dragList.splice(startIndex, 1);
+    dragList.splice(endIndex, 0, remove[0]);
+  };
+
+  const handleDragEnd = (result) => {
+    reorder(taskList, result.source.index, result.destination.index);
+    setTaskList(taskList);
+  };
   return (
     <div>
-      <DragDropContext>
+      <DragDropContext onDragEnd={handleDragEnd}>
         <Droppable droppableId="droppable">
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
-              {taskList.map((task) => (
+              {taskList.map((task, index) => (
                 <div key={task.id}>
-                  <Task task={task} taskList={taskList} setTaskList={setTaskList} />
+                  <Task task={task} index={index} taskList={taskList} setTaskList={setTaskList} />
                 </div>
               ))}
               {provided.placeholder}
