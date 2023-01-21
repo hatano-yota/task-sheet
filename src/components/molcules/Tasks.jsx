b7719f7f48febd394044a1bd2e8aec3b2e63a0fb
unreact-hatano <mizutama.hahaha@gmail.com>
2023-01-21 16:35:20 +0900

    taskコンポーネント切り出し fontawesome導入

--- src/components/molcules/Tasks.jsx
+++ src/components/molcules/Tasks.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Task } from "../atoms/Task";
 
-export const Tasks = ({ inputText, taskList }) => {
+export const Tasks = ({ taskList }) => {
   return (
     <div>
       {taskList.map((task) => (
-        <div>{task.text}</div>
+        <div key={task.id}>
+          <Task task={task} />
+        </div>
       ))}
     </div>
   );
