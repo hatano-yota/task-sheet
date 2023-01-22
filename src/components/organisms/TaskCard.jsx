cbcf12eb6a6c1e6ef0468564b4d26fd25de6daa9
unreact-hatano <mizutama.hahaha@gmail.com>
2023-01-22 18:34:04 +0900

    ドラッグ＆ドロップ、 並び替え機能追加

--- src/components/organisms/TaskCard.jsx
+++ src/components/organisms/TaskCard.jsx
@@ -3,28 +3,33 @@ import { TaskCardTitle } from "../molcules/TaskCardTitle";
 import { TaskCardDeleteButton } from "../molcules/TaskCardDeleteButton";
 import { TaskAddInput } from "../molcules/TaskAddInput";
 import { Tasks } from "../molcules/Tasks";
+import { Draggable } from "react-beautiful-dnd";
 
-export const TaskCard = ({ taskCard, taskCardList, setTaskCardList }) => {
+export const TaskCard = ({ taskCard, index, taskCardList, setTaskCardList }) => {
   const [inputText, setInputText] = useState("");
   const [taskList, setTaskList] = useState([]);
 
   return (
-    <div className="taskCard">
-      <div className="taskHeader">
-        <TaskCardTitle />
-        <TaskCardDeleteButton
-          taskCard={taskCard}
-          taskCardList={taskCardList}
-          setTaskCardList={setTaskCardList}
-        />
-      </div>
-      <TaskAddInput
-        inputText={inputText}
-        setInputText={setInputText}
-        taskList={taskList}
-        setTaskList={setTaskList}
-      />
-      <Tasks taskList={taskList} setTaskList={setTaskList} />
-    </div>
+    <Draggable draggableId={taskCard.id} index={index}>
+      {(provided) => (
+        <div className="taskCard" ref={provided.innerRef} {...provided.draggableProps}>
+          <div className="taskHeader" {...provided.dragHandleProps}>
+            <TaskCardTitle />
+            <TaskCardDeleteButton
+              taskCard={taskCard}
+              taskCardList={taskCardList}
+              setTaskCardList={setTaskCardList}
+            />
+          </div>
+          <TaskAddInput
+            inputText={inputText}
+            setInputText={setInputText}
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
+          <Tasks taskList={taskList} setTaskList={setTaskList} />
+        </div>
+      )}
+    </Draggable>
   );
 };
--- src/components/templates/TaskCards.jsx
+++ src/components/templates/TaskCards.jsx
@@ -1,22 +1,42 @@
 import React, { useState } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import { AddTaskCardButton } from "../organisms/AddTaskCardButton";
 import { TaskCard } from "../organisms/TaskCard";
 
 export const TaskCards = () => {
-  const [taskCardList, setTaskCardList] = useState([{ id: 0, draggableId: "item-00" }]);
+  const [taskCardList, setTaskCardList] = useState([{ id: "0", draggableId: "item-00" }]);
+
+  // 要修正 *重複処理
+  const reorder = (dragList, startIndex, endIndex) => {
+    // 並び替える
+    const remove = dragList.splice(startIndex, 1);
+    dragList.splice(endIndex, 0, remove[0]);
+  };
+  const handleDragEnd = (result) => {
+    reorder(taskCardList, result.source.index, result.destination.index);
+    setTaskCardList(taskCardList);
+  };
 
   return (
-    <div className="taskCardsArea">
-      {taskCardList.map((taskCard) => (
-        <TaskCard
-          key={taskCard.id}
-          taskCard={taskCard}
-          taskCardList={taskCardList}
-          setTaskCardList={setTaskCardList}
-        />
-      ))}
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <Droppable droppableId="droppable2" direction="horizontal">
+        {(provided) => (
+          <div className="taskCardsArea" {...provided.droppableProps} ref={provided.innerRef}>
+            {taskCardList.map((taskCard, index) => (
+              <TaskCard
+                key={taskCard.id}
+                taskCard={taskCard}
+                index={index}
+                taskCardList={taskCardList}
+                setTaskCardList={setTaskCardList}
+              />
+            ))}
+            {provided.placeholder}
 
-      <AddTaskCardButton taskCardList={taskCardList} setTaskCardList={setTaskCardList} />
-    </div>
+            <AddTaskCardButton taskCardList={taskCardList} setTaskCardList={setTaskCardList} />
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
